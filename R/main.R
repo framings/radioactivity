3f5b369d4faca784e0eb35559dc37bc83a0f4bbd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-11-11 22:12:13 +0000

    Add R/main.R

---
+++ R/main.R
@@ -0,0 +1,10 @@
+# Title     : main.R
+# Objective : Playground
+# Created by: greyhypotheses
+# Created on: 11/11/2022
+
+
+# Prepare the NY Coronary Artery Bypass Graft data
+source(file = 'R/preliminary/NY.R')
+NY()
+
