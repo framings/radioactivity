e02298e736a4bcb7ec366760e890bb7cf1702702
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-12-09 23:12:04 +0000

    Update R/preliminary/TeenagePregnancies.R

--- R/preliminary/TeenagePregnancies.R
+++ R/preliminary/TeenagePregnancies.R
@@ -8,7 +8,24 @@
 #'
 #'
 TeenagePregnancies <- function (){
+  
 
+  # The data
   pregnancies <- read.table(file = 'data/research/legacy/preg.data.txt', header = TRUE,  sep = '')
+  pregnancies <- dplyr::rename(pregnancies, 'POP2001' = 'POP200')
+
+  # Save; foremost, ensure the storage directory exists
+  pathstr <- file.path(getwd(), 'data', 'research', 'experimental')
+  if (!dir.exists(paths = pathstr)){
+    dir.create(path = pathstr, showWarnings = TRUE, recursive = TRUE)
+  }
+
+  utils::write.table(x = pregnancies,
+                     file = file.path(pathstr, 'teenage_pregnancies.csv'),
+                     append = FALSE,
+                     sep = ',',
+                     row.names = FALSE,
+                     col.names = TRUE,
+                     fileEncoding = 'UTF-8')
 
 }
